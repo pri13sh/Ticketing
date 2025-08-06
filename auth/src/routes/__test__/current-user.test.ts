05bd1957013bb2e582b1269c5befd7bf21e1aaf5
Pritesh <pri13sh@gmail.com>
2025-08-07 00:06:09 +0530

    Complete test cases for the auth service

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,22 +1,22 @@
 import request  from "supertest";
 import {app} from '../../app';
 
-// it('responds about the details of the current user', async ()=>{
-//     const cookie = await global.signin();
+it('responds about the details of the current user', async ()=>{
+    const cookie = await global.signin();
 
-//     const response = await request(app)
-//     .post('/api/users/currentuser')
-//     .set('Cookie', cookie)
-//     .send()
-//     .expect(200);
+    const response = await request(app)
+    .get('/api/users/currentuser')
+    .set('Cookie', cookie)
+    .send()
+    .expect(200);
 
-//     expect(response.body.currentUser.email).toEqual('test@test.com');
+    expect(response.body.currentUser.email).toEqual('test@test.com');
 
-// })
+})
 
 it('responds with null if not authenticated', async ()=>{
     const response = await request(app)
-    .post('/api/users/currentuser')
+    .get('/api/users/currentuser')
     .send()
     .expect(200);
 
