2303209b1105023dcf600d456ec5601c2be02a06
Pritesh <pri13sh@gmail.com>
2025-08-09 10:37:41 +0530

    signup implementation in react

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,17 +1,21 @@
 import { useState } from 'react';
 import axios from 'axios';
+import useRequest from '../../hooks/use-request';
 
 export default () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-
+    const {doRequest,errors} = useRequest({
+        url:'/api/users/signup',
+        method:'post',
+        body:{
+            email,password
+        }
+    })
     const onSubmit = async (event)=>{
         event.preventDefault();
-        const response = await axios.post('/api/users/signup',{
-            email,password
-        })
-
-        console.log(response.data)
+        
+        doRequest();
     }
     return (
         <form onSubmit={onSubmit}>
@@ -24,6 +28,7 @@ export default () => {
                 <label>Password</label>
                 <input value={password} onChange={e => setPassword(e.target.value)} type="password" className="form-control" />
             </div>
+            { errors}
             <button className="btn btn-primary">Sign Up</button>
         </form>
     );
