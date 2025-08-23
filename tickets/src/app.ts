d8eec1c62ddd9bbac66832ad95ae5947c9f4ec9a
Pritesh <pri13sh@gmail.com>
2025-08-23 18:04:33 +0530

    Tickets service setup, deployment|service|skaffold

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,11 +3,6 @@ import 'express-async-errors'
 import { json } from 'body-parser';
 
 import cookieSession from "cookie-session";
-
-import {currentUserRouter} from './routes/current-user';
-import {signinRouter} from './routes/signin';
-import {signoutRouter} from './routes/signout';
-import {signupRouter} from './routes/signup';
 import { errorHandler,NotFoundError } from "@pri13sh/common";
 const app = express();
 app.set('trust proxy',true);
@@ -16,14 +11,8 @@ app.use(
     cookieSession({
         signed: false,
         secure:  process.env.NODE_ENV !== 'test'
-        // secure: false
     })
 )
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
 
 
 app.all('*',async (req, res) =>{
