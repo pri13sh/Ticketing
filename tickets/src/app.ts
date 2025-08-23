3d675df2eb4c9ebbf5786a47a3b07a7c24c9abba
Pritesh <pri13sh@gmail.com>
2025-08-23 22:48:54 +0530

    test cases for ticket service

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import 'express-async-errors'
 import { json } from 'body-parser';
-
+import { createTicketRouter } from "./routes/new";
 import cookieSession from "cookie-session";
-import { errorHandler,NotFoundError } from "@pri13sh/common";
+import { errorHandler,NotFoundError , currentUser} from "@pri13sh/common";
+
 const app = express();
 app.set('trust proxy',true);
 app.use(json());
@@ -13,7 +14,8 @@ app.use(
         secure:  process.env.NODE_ENV !== 'test'
     })
 )
-
+app.use(currentUser)
+app.use(createTicketRouter);
 
 app.all('*',async (req, res) =>{
     throw new NotFoundError();
