80222d1ebcf54a8a3d85d7f94f93272f2d100c38
Pritesh <pri13sh@gmail.com>
2025-08-24 11:48:48 +0530

    test cases for ticket service

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,9 +2,10 @@ import {MongoMemoryServer} from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import request from 'supertest';
 import {app} from '../app';
+import jwt from 'jsonwebtoken';
 
 declare global{
-    var signin: () => Promise<string[]>;
+    var signin: () => string[];
 }
 let mongo:any;
 beforeAll(async ()=>{
@@ -28,20 +29,25 @@ afterAll(async ()=>{
     await mongoose.connection.close();
 })
 
-global.signin = async ()=>{
-    const email = 'test@test.com';
-    const password = 'password';
-     
-    const response = await request(app)
-    .post('/api/users/signup')
-    .send({
-        email,password
-    })
-    .expect(201);
-
-    const cookie = response.get('Set-Cookie');
-    if (!cookie) {
-        throw new Error('Cookie not set');
+global.signin =  ()=>{
+    // Build a jwt paload. {id , email}
+    const paload ={
+        id: '23edwdsfdsf211',
+        email: 'test@test.com'
     }
-    return cookie;
+
+    //create  the jwt
+    const token = jwt.sign(paload,process.env.JWT_KEY!)
+
+    //build session object {jwt: M_JWT}
+    const session = { jwt: token };
+
+    // turn that session into json
+    const sessionJSON = JSON.stringify(session);
+
+    // take json and create it as base64
+    const base64 = Buffer.from(sessionJSON).toString('base64');
+
+    //return a string thats the cookie with the emcoded data
+    return [`session=${base64}`];
 }
